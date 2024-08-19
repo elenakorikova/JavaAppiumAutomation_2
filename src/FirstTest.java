f4a071e51785dc7c8017e7fff67ff746458b1085
Елена Корикова <elenakorikova@MacBook-Pro-Aleksej.local>
2024-08-19 07:44:44 +0300

    Initial commit

---
+++ src/FirstTest.java
@@ -0,0 +1,39 @@
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.URL;
+
+public class FirstTest {
+
+    private AppiumDriver driver;
+
+    @Before
+    public void setUp() throws Exception
+    {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("platformName","Android");
+        capabilities.setCapability("deviceName","Google_Nexus_5");
+        capabilities.setCapability("platformVersion","11");
+        capabilities.setCapability("appium:automationName","UiAutomator2");
+        capabilities.setCapability("appPackage","org.wikipedia");
+        capabilities.setCapability("appActivity",".main.MainActivity");
+        capabilities.setCapability("app","/Users/elenakorikova/Desktop/JavaAppiumAutomation/JavaAppiumAutomation_2/JavaAppiumAutomation_2/apks/org.wikipedia_50467_apps.evozi.com.apk");
+
+        driver = new AndroidDriver(new URL("http://127.0.0.1:4723"), capabilities);
+    }
+    @After
+    public void tearDown()
+    {
+        driver.quit();
+    }
+
+    @Test
+    public void firstTest()
+    {
+        System.out.println("First test run");
+    }
+}
