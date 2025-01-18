7de91f1d21dbf39256d04c5d0ed67659f02b4798
elenakorikova <korikova.korikova@gmail.com>
2025-01-18 20:57:37 +0300

    first test "find and click"

--- src/lib/CoreTestCase.java
+++ src/lib/CoreTestCase.java
@@ -13,6 +13,9 @@ import java.util.Map;
 
 public class CoreTestCase extends TestCase {
 
+    private static final String PLATFORM_IOS = "ios";
+    private static final String PLATFORM_ANDROID = "android";
+
     protected AppiumDriver driver;
     private static String AppiumURL = "http://127.0.0.1:4723";
 
@@ -21,15 +24,7 @@ public class CoreTestCase extends TestCase {
 
         super.setUp();
 
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("platformName", "Android");
-        capabilities.setCapability("deviceName", "Google_Nexus_5");
-        capabilities.setCapability("platformVersion", "11");
-        capabilities.setCapability("appium:automationName", "UiAutomator2");
-        capabilities.setCapability("appPackage", "org.wikipedia");
-        capabilities.setCapability("appActivity", ".main.MainActivity");
-        capabilities.setCapability("app", "/Users/elenakorikova/Desktop/JavaAppiumAutomation/JavaAppiumAutomation_2/JavaAppiumAutomation_2/apks/org.wikipedia_50467_apps.evozi.com.apk");
-
+        DesiredCapabilities capabilities = this.getCapabilitiesByPlatformEnv();
         driver = new AndroidDriver(new URL(AppiumURL), capabilities);
         this.rotateScreenPortrait();
 
@@ -80,5 +75,28 @@ public class CoreTestCase extends TestCase {
         driver.runAppInBackground(seconds);
     }
 
+    private DesiredCapabilities getCapabilitiesByPlatformEnv() throws Exception {
+        String platform = System.getenv("PLATFORM");
+        DesiredCapabilities capabilities = new DesiredCapabilities();
 
+        if (platform.equals(PLATFORM_ANDROID)) {
+            capabilities.setCapability("platformName", "Android");
+            capabilities.setCapability("deviceName", "Google_Nexus_5");
+            capabilities.setCapability("platformVersion", "11");
+            capabilities.setCapability("appium:automationName", "UiAutomator2");
+            capabilities.setCapability("appPackage", "org.wikipedia");
+            capabilities.setCapability("appActivity", ".main.MainActivity");
+            capabilities.setCapability("app", "/Users/elenakorikova/Desktop/JavaAppiumAutomation/JavaAppiumAutomation_2/JavaAppiumAutomation_2/apks/org.wikipedia_50467_apps.evozi.com.apk");
+        } else if (platform.equals(PLATFORM_IOS)) {
+            capabilities.setCapability("platformName", "iOS");
+            capabilities.setCapability("deviceName", "iPhone 16 Pro");
+            capabilities.setCapability("platformVersion", "18.2");
+            capabilities.setCapability("appium:automationName", "XCUITest");
+            capabilities.setCapability("app", "/Users/elenakorikova/Desktop/JavaAppiumAutomation/JavaAppiumAutomation_2/JavaAppiumAutomation_2/apks/Wikipedia.app");
+            capabilities.setCapability("bundleId", "org.wikimedia.wikipedia");
+        } else {
+            throw new Exception("Cannot get run platform from env variable. Platform value " + platform);
+        }
+        return capabilities;
+    }
 }
